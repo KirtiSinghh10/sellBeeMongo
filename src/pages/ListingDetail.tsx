bc6ce2fc33c8c4fb73ab66cf795179a5458edaf1
Kirti Singh <kirtisingh.cs24@bmsce.ac.in>
2026-01-02 18:40:23 +0530

    Added api.ts

--- src/pages/ListingDetail.tsx
+++ src/pages/ListingDetail.tsx
@@ -6,6 +6,8 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { ArrowLeft, Mail, Loader2, MessageCircle } from "lucide-react";
 import ListingImageCarousel from "@/components/ListingImageCarousel";
+const API = import.meta.env.VITE_API_URL;
+
 
 /* ================= TYPES ================= */
 interface ListingImage {
@@ -65,7 +67,7 @@ const ListingDetail = () => {
 
     const fetchListing = async () => {
       try {
-        const res = await fetch(`http://localhost:5000/products/${id}`);
+        const res = await fetch(`${API}/products/${id}`);
         if (!res.ok) throw new Error("Listing not found");
 
         const data = await res.json();
